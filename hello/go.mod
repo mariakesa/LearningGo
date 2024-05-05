a889b3404aa4750955421d909dfbd715c9256b73
Maria <mariarosekesa@gmail.com>
2024-05-05 21:24:41 +0300

    importing packages

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,7 @@
 module example/hello
 
 go 1.18
+
+replace testpack => ../testpack
+
+require testpack v0.0.0-00010101000000-000000000000
--- hello/my_pack.go
+++ hello/my_pack.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"testpack"
 )
 
 func MyExtension() {
 	fmt.Println("Hello, World!")
 	maria := "Today!"
 	fmt.Printf("Rasmus is hacking the world! %s \n", maria)
+	testpack.Hello("Rasmus")
 }
